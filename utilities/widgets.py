038aad059aeff5a7a3e98b40e5f176ca8f0b5ca6
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2010-07-18 14:44:37 +0600

    Fix ImagePreviewWidget for empty values

--- utilities/widgets.py
+++ utilities/widgets.py
@@ -18,29 +18,29 @@ class ImagePreviewWidget(AdminFileWidget):
         output.append(super(AdminFileWidget, self).render(name, value, attrs))
         instance = getattr(value, 'instance', None)
         if instance is not None:
-            field = value.field.name
-            if self.thumb_field is None:
-                thumb_field = field
-            else:
-                thumb_field = self.thumb_field
-            if self.thumb_size is None:
-                sizes = ''
-            else:
-                cur_width = getattr(instance, field).width
-                cur_height = getattr(instance, field).height
-                thumb_width, thumb_height = self.thumb_size
-                if not thumb_width is None and not thumb_height is None:
-                    ratio = min(float(thumb_width) / cur_width,
-                                float(thumb_height) / cur_height)
+            field = getattr(instance, value.field.name, None)
+            if field:
+                if self.thumb_field is None:
+                    thumb_field = field
                 else:
-                    if thumb_width is None:
-                        ratio = float(thumb_height) / cur_height
+                    thumb_field = getattr(instance, self.thumb_field, None)
+                if self.thumb_size is None:
+                    style = ''
+                else:
+                    cur_width, cur_height = field.width, field.height
+                    thumb_width, thumb_height = self.thumb_size
+                    if not thumb_width is None and not thumb_height is None:
+                        ratio = min(float(thumb_width) / cur_width,
+                                    float(thumb_height) / cur_height)
                     else:
-                        ratio = float(thumb_width) / cur_width
-                new_size = (int(round(cur_width * ratio)), int(round(cur_height * ratio)))
-                if new_size[0] > cur_width or new_size[1] > cur_height:
-                    new_size = self.thumb_size
-                sizes = 'width: %spx; height: %spx;' % new_size
-            output.append('<br /><a target="_blank" href="%s"><img src="%s" alt="%s" style="%s" /></a>' % \
-                (getattr(instance, field).url, getattr(instance, thumb_field).url, getattr(instance, field), sizes))
+                        if thumb_width is None:
+                            ratio = float(thumb_height) / cur_height
+                        else:
+                            ratio = float(thumb_width) / cur_width
+                    new_size = (int(round(cur_width * ratio)), int(round(cur_height * ratio)))
+                    if new_size[0] > cur_width or new_size[1] > cur_height:
+                        new_size = self.thumb_size
+                    style = 'style="width: %spx; height: %spx; "' % new_size
+                output.append('<br /><a target="_blank" href="%s"><img src="%s" alt="%s" %s/></a>' % \
+                    (field.url, thumb_field.url, field, style))
         return mark_safe(u''.join(output))
