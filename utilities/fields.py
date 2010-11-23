0e48b2a44ced3f97ad71123b57caa178fd5571dc
Evgeniy Gulitsyn <evgeniy.gulitsyn@redsolution.ru>
2010-11-23 11:15:47 +0500

    add SplitDateField

--- utilities/fields.py
+++ utilities/fields.py
@@ -3,7 +3,9 @@
 from django import forms
 from django.db import models
 from django.contrib.admin.widgets import AdminFileWidget
+from django.utils import dates
 from utilities.widgets import ImagePreviewWidget
+import datetime
 
 class ImagePreviewFormField(forms.ImageField):
     widget = ImagePreviewWidget
@@ -30,3 +32,86 @@ class ImagePreviewField(models.ImageField):
             defaults['widget'] = ImagePreviewWidget(thumb_field=self.thumb_field, thumb_size=self.thumb_size)
 
         return super(ImagePreviewField, self).formfield(**defaults)
+
+#    SplitDateField
+EMPTY = [('', u'---')]
+DAYS = [(day, '%02d' % day) for day in xrange(1, 32)]
+MONTHS = [(month, dates.MONTHS[month]) for month in xrange(1, 13)]
+DEFAULT_FROM_YEAR = 1930
+
+def widget_factory(years):
+    class SplitDateWidget(forms.MultiWidget):
+        def __init__(self, attrs=None):
+            widgets = (
+                forms.Select(attrs=None, choices=EMPTY + DAYS),
+                forms.Select(attrs=None, choices=EMPTY + MONTHS),
+                forms.Select(attrs=None, choices=EMPTY + years),
+            )
+            super(SplitDateWidget, self).__init__(widgets, attrs)
+
+        def decompress(self, value):
+            if value:
+                return [value.day, value.month, value.year]
+            return [None, None, None]
+    return  SplitDateWidget
+
+class SplitDateFormField(forms.MultiValueField):
+
+    def __init__(self, from_date=datetime.date(DEFAULT_FROM_YEAR, 01, 01),
+        till_date=datetime.date.today, reverse=False, *args, **kwargs):
+        if callable(from_date):
+            from_date = from_date()
+        if callable(till_date):
+            till_date = till_date()
+        self.from_date = from_date
+        self.till_date = till_date
+        from_year = from_date.year
+        till_year = till_date.year
+        years = [(year, '%04d' % year) for year in xrange(till_year,
+            from_year - 1, -1)]
+        if reverse:
+            years.reverse()
+        errors = self.default_error_messages.copy()
+        if 'error_messages' in kwargs:
+            errors.update(kwargs['error_messages'])
+        kwargs['widget'] = widget_factory(years)
+        fields = (
+            forms.ChoiceField(choices=DAYS),
+            forms.ChoiceField(choices=MONTHS),
+            forms.ChoiceField(choices=years),
+        )
+        super(SplitDateFormField, self).__init__(fields, *args, **kwargs)
+
+    def compress(self, value_list):
+        if value_list:
+            for value in value_list:
+                if value in forms.fields.EMPTY_VALUES:
+                    raise forms.ValidationError(forms.SplitDateTimeField.default_error_messages['invalid_date'])
+            kwargs = {
+                'day': int(value_list[0]),
+                'month': int(value_list[1]),
+                'year': int(value_list[2]),
+            }
+            try:
+                date = datetime.date(**kwargs)
+                if date > self.till_date or date < self.from_date:
+                    raise forms.ValidationError(forms.SplitDateTimeField.default_error_messages['invalid_date'])
+                else:
+                    return date
+            except ValueError:
+                raise forms.ValidationError(forms.SplitDateTimeField.default_error_messages['invalid_date'])
+        return None
+
+class SplitDateField(models.DateField):
+    def __init__(self, from_date=datetime.date(DEFAULT_FROM_YEAR, 01, 01),
+        till_date=datetime.date.today, reverse=False, *args, **kwargs):
+        self.from_date = from_date
+        self.till_date = till_date
+        self.reverse = reverse
+        super(SplitDateField, self).__init__(*args, **kwargs)
+
+    def formfield(self, ** kwargs):
+        defaults = {'form_class': SplitDateFormField, }
+        defaults.update(kwargs)
+        return super(SplitDateField, self).formfield(from_date=self.from_date,
+            till_date=self.till_date, reverse=self.reverse, ** defaults)
